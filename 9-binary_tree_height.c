3bad1d8fa8bd88e93af30429703cd78a03c020be
johnahumanbeing <mseewak@gmail.com>
2024-01-04 23:17:48 +0300

    function that measures the height of a binary tree (fixed line 17 error)

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,7 +14,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	return (0);
 
 	height_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	height_right = tree->right ? +binary_tree_height(tree->right) : 0;
+	height_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
 	return (height_left > height_right ? height_left : height_right);
 
